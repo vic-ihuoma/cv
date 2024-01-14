794b9324337977fcdeb9fdce625a59aa7424d527
Victor Ihuoma <victorihuoma@Victors-MacBook-Pro.local>
2024-01-14 09:36:40 +0100

    fix: ts-config

--- src/images/logos/index.ts
+++ src/images/logos/index.ts
@@ -1,6 +1,6 @@
-import Danfoss from "./danfoss.svg";
-import SDU from "./sdu_logo.png"
-import RecursiveMusings from "./recursive_musings.png";
+import Danfoss from 'src/images/logos/danfoss.svg';
+import SDU from "src/images/logos/sdu_logo.png"
+import RecursiveMusings from "src/images/logos/recursive_musings.png";
 
 export {
   Danfoss,
