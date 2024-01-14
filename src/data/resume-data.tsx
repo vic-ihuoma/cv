6e70d48b82a6b1db1fdeaebf3a4799cc59ca2e9e
victor <victor.ihuoma@danfoss.com>
2024-01-14 08:23:25 +0100

    chore(dev): updated cv

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,107 +1,78 @@
 import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
+  Danfoss,
+  SDU,
+  RecursiveMusings,
 } from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Victor U. Ihuoma",
+  initials: "VUI",
+  location: "Copenhagen, Denmaark, CET",
+  locationLink: "https://www.google.com/maps/place/Copenhagen",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Digital Products Enthusiast focused on building scalable, resilient services with extra attention to detail",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "As a Software Developer, I have successfully taken multiple digital products from 0 to 1. I am adept at developing and deploying scalable products, communicating effectively and ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, Node.js, and PostgresQL. I am no stranger to a fast paced environment and possess 4+ years of experience working in a distributed team.",
+  avatarUrl: "https://avatars.githubusercontent.com/u/121028057?v=4",
+  personalWebsiteUrl: "https://recursivemusings.me",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "victoruihuoma@icloud.com",
+    tel: "+4527582808",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/vic-ihuoma",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/victor-ihuoma/",
         icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "University of Southern Denmark",
+      degree: "Masters Degree in Mechatronics Engineering",
+      start: "2017",
+      end: "2019",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
+      company: "Danfoss Climate Solutions",
+      link: "https://www.danfoss.com/en-us/about-danfoss/our-businesses/climate-solutions/",
+      badges: ["FullTime"],
+      title: "Software Developer",
+      logo: Danfoss,
       start: "2021",
-      end: "2024",
+      end: "-",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "Implemented new features including some that enabled customers remotely configure 1000s of critical devices securely and reliably, worked on improving the way developers ship the code through test driven development, Technologies: Node.js, TypeScript, Kafka, Docker, Kubernetes, Postgres, Redis",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
+      company: "Danfoss A/S",
+      link: "https://www.danfoss.com/en-us/",
+      badges: ["FullTime"],
+      title: "Innovation Engineer",
+      logo: Danfoss,
+      start: "2019",
       end: "2021",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "Designed and implemented scalable data pipelines to power predictive maintenance services and enable BI teams. Technologies: Python, Azure Services, Argo CD",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
+      company: "University of Southern Denmark",
+      link: "https://www.sdu.dk/en",
       badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      title: "Academic Advisor",
+      logo: SDU,
+      start: "2018",
+      end: "2019",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+        "Designed and developed a pricing solution algorithm and dashboard for an electronic component manufacturer. Technologies: Python, Plotly",
     },
   ],
   skills: [
@@ -109,170 +80,49 @@ export const RESUME_DATA = {
     "TypeScript",
     "React/Next.js/Remix",
     "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "PostgresQL",
+    "Go",
+    "NestJs",
+    "Kubernetes",
+    "Docker",
+    "Azure",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Automatic Demand Response",
       techStack: [
-        "Side Project",
         "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "NestJs",
+        "Redis"
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      description: "A platform to allow customers respond to their utility providers incentives to reduce their energy consumption during peak hours",
+      logo: Danfoss,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "danfoss.com",
+        href: "https://www.danfoss.com/en-us/service-and-support/case-stories/cf/engineering-tomorrow-helps-transform-supermarkets-into-smart-stores-for-demand-response/",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "Global Configuation Management",
+      techStack: ["TypeScript", "Node.js", "Kafka", "PostgresQL", "BullMQ"],
       description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
+        "Web service to enable users remotely configure 1000s of critical devices securely and reliably",
+      logo: Danfoss,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "danfoss.com",
+        href: "https://danfoss.com",
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
+      title: "recursivemusings.me",
+      techStack: ["Side Project", "Next.js", "MDX", "React"],
       description:
         "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
+      logo: RecursiveMusings,
       link: {
         label: "github.com",
-        href: "https://jarocki.me/",
+        href: "https://recursivemusings.me/",
       },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
